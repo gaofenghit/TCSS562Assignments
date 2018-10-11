919db8a33a622d19e6c904f61b86607532003839
Frank <gaofenghit@gmail.com>
2018-10-11 07:37:16 +0000

    fix bug.

--- 2/weather.sh
+++ 2/weather.sh
@@ -8,13 +8,11 @@ cd `dirname $0`
 set -eu
 
 key=ac63672a3ce44460831b60b5294f47e3
-
 res=""
 
 if [ ! -f ".myipaddr" ]
 then
   echo "CALLING API TO QUERY MY IP"
-#  loc=`curl -u $TOKEN: ipinfo.io 2>/dev/null | jq '.loc' | sed 's/\"//g'`
   loc=`curl ipinfo.io 2>/dev/null | jq '.loc' | sed 's/\"//g'`
   lat=`echo $loc | awk -F ',' '{print $1}'`
   lon=`echo $loc | awk -F ',' '{print $2}'`
@@ -28,10 +26,6 @@ fi
 
 lat=`echo $res | jq '.| .lat'`
 lon=`echo $res | jq '.| .lon'`
-
 echo "Forecast for my lat=${lat}, lon=${lon}"
-
-echo $res | jq -r '.data[] | "Forecast for \(.valid_date) HI: \(.min_temp)°c LOW: \(.max_temp)°c" ' 
-
-
+echo $res | jq -r '.data[] | "Forecast for \(.valid_date) HI: \(.max_temp)°c LOW: \(.min_temp)°c" ' 
 
